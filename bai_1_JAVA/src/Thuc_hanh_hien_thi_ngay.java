cda65fe004a5c359bb553da31b86f0f27c4eb9f5
Your Namenguyenvandong0798 <you@example.comnguyenvandong0798@gmail.com>
2021-10-01 11:25:24 +0700

    commit

---
+++ bai_1_JAVA/src/Thuc_hanh_hien_thi_ngay.java
@@ -0,0 +1,10 @@
+
+    import java.util.Date;
+
+    public class Thuc_hanh_hien_thi_ngay {
+        public static void main(String[] args) {
+            Date now = new Date(); // Lấy ra thời gian hiện tại
+
+            System.out.println("Now is: " + now);
+        }
+    }
