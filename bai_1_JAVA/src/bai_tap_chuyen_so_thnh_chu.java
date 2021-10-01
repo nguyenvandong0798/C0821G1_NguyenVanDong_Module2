71a04495ce35294f7296e4da8f3159e2cbdd9f88
Your Namenguyenvandong0798 <you@example.comnguyenvandong0798@gmail.com>
2021-10-01 22:23:55 +0700

    commit

--- bai_1_JAVA/src/bai_tap_chuyen_so_thnh_chu.java
+++ bai_1_JAVA/src/bai_tap_chuyen_so_thnh_chu.java
@@ -1,43 +1,118 @@
-import javax.swing.*;
-import java.text.BreakIterator;
+
 import java.util.Scanner;
 
 public class bai_tap_chuyen_so_thnh_chu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("ener a munber");
+        System.out.println("enter a munber");
         int number = sc.nextInt();
-        switch (number){
+        int hundreds = number / 100;
+        int dozens = (number - (hundreds * 100)) / 10;
+        int units = (number - (hundreds * 100 + dozens * 10));
+        String displayNum;
+        String displayHun;
+        String displayDoz;
+        switch (dozens){
             case 1:
-                System.out.println("one");
+                displayDoz = " ten";
                 break;
             case 2:
-                System.out.println("two");
+                displayDoz = "twenty";
                 break;
             case 3:
-                System.out.println("three");
+                displayDoz = "thirty";
                 break;
             case 4:
-                System.out.println("four");
+                displayDoz = "forty";
                 break;
             case 5:
-                System.out.println("five");
+                displayDoz = "fifty";
                 break;
             case 6:
-                System.out.println("six");
+                displayDoz = "sixty";
                 break;
             case 7:
-                System.out.println("seven");
+                displayDoz = "seventy";
                 break;
             case 8:
-                System.out.println("eight");
+                displayDoz = "eighty";
                 break;
             case 9:
-                System.out.println("nine");
+                displayDoz = "ninety";
                 break;
             default:
-                System.out.println("ou of ability");
-
+                displayDoz = "";
+                break;
+        }
+        System.out.print(displayDoz);
+        switch (hundreds){
+            case 1:
+                displayHun = " one hundreds";
+                break;
+            case 2:
+                displayHun = "two hundreds";
+                break;
+            case 3:
+                displayHun = "three hundreds";
+                break;
+            case 4:
+                displayHun = "four hundreds";
+                break;
+            case 5:
+                displayHun = "five hundreds";
+                break;
+            case 6:
+                displayHun = "six hundreds";
+                break;
+            case 7:
+                displayHun = "seven hundreds";
+                break;
+            case 8:
+                displayHun = "eight hundreds";
+                break;
+            case 9:
+                displayHun = "nine hundreds";
+                break;
+            default:
+                displayHun = "";
+                break;
+        }
+        System.out.print(displayHun);
+        switch (units) {
+            case 1:
+                displayNum = "one";
+                break;
+            case 2:
+                displayNum = "two";
+                break;
+            case 3:
+                displayNum = "three";
+                break;
+            case 4:
+                displayNum = "four";
+                break;
+            case 5:
+                displayNum = "five";
+                break;
+            case 6:
+                displayNum = "six";
+                break;
+            case 7:
+                displayNum = "seven";
+                break;
+            case 8:
+                displayNum = "eight";
+                break;
+            case 9:
+                displayNum = "nine";
+                break;
+            default:
+                displayNum = "";
+        }if (displayNum == ""){
+            System.out.print("out of ability");
+        }else{
+            System.out.print(displayNum);
+        }
         }
     }
-}
+
