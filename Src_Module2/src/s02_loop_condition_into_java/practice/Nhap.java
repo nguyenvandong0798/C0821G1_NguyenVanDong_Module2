898bededf491ece15bc39a95c73fa41bf230cf6a
Your Namenguyenvandong0798 <you@example.comnguyenvandong0798@gmail.com>
2021-10-06 08:31:21 +0700

    commit

--- Src_Module2/src/s02_loop_condition_into_java/practice/Nhap.java
+++ Src_Module2/src/s02_loop_condition_into_java/practice/Nhap.java
@@ -4,18 +4,46 @@ import java.util.Scanner;
 
 public class Nhap {
     public static void main(String[] args) {
-        int [] num = {1,8,2,4,5};
-        float avg = average(num);
-        System.out.println("gia tri trung binh: " + avg);
-    }
+//        int [] num = {1,8,2,4,5};
+//        float avg = average(num);
+//        System.out.println("gia tri trung binh: " + avg);
+//    }
+//
+//
+//    public static float average(int[] arr){
+//        int sum = 0;
+//        for (int i = 0; i < arr.length; i++){
+//            sum +=arr[i];
+//        }
+//        return (float) sum / arr.length;
 
 
-    public static float average(int[] arr){
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++){
-            sum +=arr[i];
+
+//        int[] a = {1,9,3,2};
+//        for(int i=0;i<a.length-1;i++){
+//            for(int j=i+1;j<a.length;j++){
+//                if(a[i] > a[j]){
+//                    int x = a[i];
+//                    a[i] = a[j];
+//                    a[j] = x;
+//                }
+//            }
+//        }
+
+
+        int[] x = {5, 6, 7, 8};
+
+        for(int i = 0; i < x.length; i++){
+
+            if(i % 2 == 0){
+
+                continue;
+
+            }
+
+            System.out.print(i);
+
         }
-        return (float) sum / arr.length;
     }
 }
 
--- Src_Module2/src/s03_array/exercise/AddIndexInArray.java
+++ Src_Module2/src/s03_array/exercise/AddIndexInArray.java
@@ -10,17 +10,13 @@ public class AddIndexInArray {
         int number = sc.nextInt();
         System.out.println("enter index:");
         int index = sc.nextInt();
-        int i;
-        for (i = 0; i < array.length; i++) {
+        for (int i = array.length - 1; i < index; i--) {
             if (array[index] == array[i]) ;
-            array[i] = array[i + 1];
+            array[i] = array[i - 1];
         }
+        array[index] = number;
         for (int j = 0; j < array.length; j++){
-            if (array[j] == array[index]){
-                number = array[j];
-                }
-            System.out.println(array[i]);
+                System.out.println(array[j]);
             }
-
         }
     }
